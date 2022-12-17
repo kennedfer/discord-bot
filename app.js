6ad615485f0414a7db02fb93d337f3c6bbfd5051
kennedfer <kenned14santos@gmail.com>
2022-12-17 18:38:35 -0300

    simple bot

---
+++ app.js
@@ -0,0 +1,53 @@
+require('dotenv').config(); //initialize dotenv
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * 100);
+}
+
+function startGame(){
+    randomNumber = getRandomNumber();
+    isGameRunning = true;
+}
+
+function checkNumber(number, randomNumber){
+    return number>randomNumber? {msg: "is smaller", isCorrect: false }: (number < randomNumber? {msg: "is greater", isCorrect: false } : {msg: "ok, its correct, "+number+" is the number", isCorrect: true });
+}
+
+const { Client, GatewayIntentBits } = require('discord.js');
+var isGameRunning;
+var randomNumber = getRandomNumber();
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
+client.on('ready', () => {
+    console.log(`Is runnnig`);
+});
+
+client.on('messageCreate', response=> {
+    var message = response.content;
+    if(isGameRunning){
+        if(!response.author.bot){
+            var result = checkNumber(message, randomNumber);
+            if(result.isCorrect){
+                isGameRunning = false;
+            }
+
+            response.reply(result.msg);
+        }
+    }else{
+        if(message === "let me try"){
+            response.reply('lesgo, guess the number');
+            startGame();
+        }
+    }
+});
+
+//make sure this line is the last line
+client.login(process.env.CLIENT_TOKEN);
